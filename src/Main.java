118d0c1101eb8dd807798bac553eb65a4e079d04
Amam Khokhar <amamqaiser@gmail.com>
2025-06-02 15:11:22 +0500

    Added a function

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+            check1();
     }
-
+        static void check1() {
+            String f = new String("amam");
+            String s = new String("amam");
+            System.out.println("Result of Using .equals() ");
+            System.out.println(f.equals(s));
+            System.out.println("Result of Using == ");
+            System.out.println(f == s);
+        }
 
 }
